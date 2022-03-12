2c91ccc826c315315d2d57bea9b579a86292b6fc
saifullah rahman <saiflll284@gmail.com>
2022-03-12 13:37:38 +0530

    added some dependencies

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,4 +1,6 @@
 import React from "react";
+// import {registerUser} from '../../features/auth/authSlice'
+import {useDispatch} from 'react-redux';
 
 const Login = () => {
   return <div>Login</div>;
