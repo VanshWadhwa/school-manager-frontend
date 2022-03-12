351d3b9cbddf4c4c4ba55b1bed6eb72409912081
saifullah rahman <saiflll284@gmail.com>
2022-03-12 13:53:53 +0530

    test

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,9 +1,9 @@
 import React from "react";
-// import {registerUser} from '../../features/auth/authSlice'
+import {registerUser} from '../../features/auth/authSlice'
 import {useDispatch} from 'react-redux';
 
 const Login = () => {
-  return <div>Login</div>;
+  return <div>Login lol</div>;
 };
 
 export default Login;
